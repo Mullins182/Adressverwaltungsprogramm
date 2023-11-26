f480c99e9f82b6eec71e4be75482e77b33ed654f
Mullins182 <Mullins182@t-online.de>
2023-11-26 08:03:53 +0100

    Klassen erstellt ... FileOutput Methode erstellt und getestet

--- Adressverwaltungsprogramm/MainWindow.xaml.cs
+++ Adressverwaltungsprogramm/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Adressverwaltungsprogramm.Controller;
+using Adressverwaltungsprogramm.Models;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,7 +25,22 @@ namespace Adressverwaltungsprogramm
 
         private void saveData_Click(object sender, RoutedEventArgs e)
         {
+            string filePath = "Kontaktdaten.dat";
+            FormData contactData = new FormData();
+            List<FormData> writeData = new List<FormData>();
 
+            contactData.Vorname = Vorname.Text;
+            contactData.Nachname = Nachname.Text;
+            contactData.Strasse = Straße.Text;
+            contactData.Postleitzahl = PLZ.Text;
+            contactData.Ort = Ort.Text;
+            contactData.Telefonnummer = Telefonnummer.Text;
+            contactData.Mobilnummer = Handynummer.Text;
+            contactData.Email = Emailadresse.Text;
+
+            writeData.Add(contactData);
+
+            FileAccess.WriteData(writeData, filePath);
         }
 
         private void saveData_MouseEnter(object sender, MouseEventArgs e)
---
+++ Adressverwaltungsprogramm/Models/FormData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adressverwaltungsprogramm.Models
+{
+    public class FormData
+    {
+        public string? Vorname         { get; set; }
+        public string? Nachname        { get; set; }
+        public string? Strasse         { get; set; }
+        public string? Postleitzahl    { get; set; }
+        public string? Ort             { get; set; }
+        public string? Telefonnummer   { get; set; }
+        public string? Mobilnummer     { get; set; }
+        public string? Email           { get; set; }
+    }
+}
