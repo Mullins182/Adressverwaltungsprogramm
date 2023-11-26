70778b15608448713797e5a8be65d066908f8dbd
Mullins182 <Mullins182@t-online.de>
2023-11-26 09:20:30 +0100

    Datensatz löschen Funktion hinzugefügt und getestet !

--- Adressverwaltungsprogramm/MainWindow.xaml.cs
+++ Adressverwaltungsprogramm/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Adressverwaltungsprogramm.Controller;
 using Adressverwaltungsprogramm.Models;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Adressverwaltungsprogramm
 {
@@ -18,34 +20,90 @@ namespace Adressverwaltungsprogramm
     /// </summary>
     public partial class MainWindow : Window
     {
+        string filePath = "Kontaktdaten.dat";
         public MainWindow()
         {
             InitializeComponent();
+
+            listBoxOutput();
         }
 
         private void saveData_Click(object sender, RoutedEventArgs e)
         {
-            string filePath = "Kontaktdaten.dat";
-            FormData contactData = new FormData();
-            List<FormData> writeData = new List<FormData>();
+            FormData contactData        = new FormData();
+            List<FormData> writeData    = new List<FormData>();
 
-            contactData.Vorname = Vorname.Text;
-            contactData.Nachname = Nachname.Text;
-            contactData.Strasse = Straße.Text;
-            contactData.Postleitzahl = PLZ.Text;
-            contactData.Ort = Ort.Text;
-            contactData.Telefonnummer = Telefonnummer.Text;
-            contactData.Mobilnummer = Handynummer.Text;
-            contactData.Email = Emailadresse.Text;
+            contactData.Vorname         = Vorname.Text;
+            contactData.Nachname        = Nachname.Text;
+            contactData.Strasse         = Straße.Text;
+            contactData.Postleitzahl    = PLZ.Text;
+            contactData.Ort             = Ort.Text;
+            contactData.Telefonnummer   = Telefonnummer.Text;
+            contactData.Mobilnummer     = Handynummer.Text;
+            contactData.Email           = Emailadresse.Text;
 
             writeData.Add(contactData);
 
-            FileAccess.WriteData(writeData, filePath);
+            Vorname.Text                = "";
+            Nachname.Text               = "";
+            Straße.Text                 = "";
+            PLZ.Text                    = "";
+            Ort.Text                    = "";
+            Telefonnummer.Text          = "";
+            Handynummer.Text            = "";
+            Emailadresse.Text           = "";
+
+            Controller.FileAccess.WriteData(writeData, filePath);
+
+            listBoxOutput();
+        }
+
+        // Löschen einer Zeile aus der ListBox, welche ausgewählt wurde !!!
+        private void DeleteFileLine_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> readFile = new List<string>();
+
+            if(!File.Exists(filePath))
+            {
+                throw new Exception("KEINE DATEN ZUM LÖSCHEN VORHANDEN");
+            }
+
+            var readArray = File.ReadAllLines(filePath);
+
+            foreach (var item in readArray)
+            {
+            readFile.Add(item);                
+            }
+            
+            if (FileData.SelectedItem != null)
+            {
+                if (MessageBox.Show("Wollen Sie den Eintrag wirklich löschen ???", "BESTÄTIGEN", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    int del = FileData.SelectedIndex;
+                    FileData.Items.RemoveAt(del);
+                    readFile.RemoveAt(del);
+                    File.WriteAllLines(filePath, readFile);
+                }
+            }
         }
 
         private void saveData_MouseEnter(object sender, MouseEventArgs e)
         {
 
         }
+
+        private void listBoxOutput()
+        {
+            FileData.Items.Clear();
+
+            var fileLines   = File.ReadAllLines(filePath);
+
+            foreach (var line in fileLines)
+            {
+                var output  = line.Replace(",", " ");
+                FileData.Items.Add($"{output}");
+            }
+        }
+
     }
 }
